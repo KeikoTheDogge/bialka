1bf0289acc387310e2177521c9fb97973b542fa8
Julia Weber (261659) <261659@student.pwr.edu.pl>
2024-12-12 10:20:51 +0100

    Add tittle and description for swagger documentation

---
+++ docs.py
@@ -0,0 +1,19 @@
+description = """
+### List of API: 
+
+## user
+
+* **GET user/**: show all users from database
+* **GET user/{user_id}**: show user by id
+* **POST user/**: create new user
+* **DELETE user/{user_id}**: delete user by id 
+
+## protein
+
+* **GET protein/**: show tests from database (max 100)
+* **GET protein/{protein_id}**: show test with given id
+* **POST protein/{protein_id}**: add new protein
+* **POST protein/add_fasta/{protein_id}**: add fasta file to existing protein
+* **DELETE protein/{protein_id}**: delete protein by id
+
+"""
--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from sqlalchemy.orm import Session
 from routers import routers_protein
 from routers import routers_user
 from routers import routers_uniprot
+from docs import description
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Białkowy backend 🤠",
+    description=description
+)
+
 app.include_router(routers_protein.router)
 app.include_router(routers_user.router)
 app.include_router(routers_uniprot.router)
